01be2bb33a094dc14982f59e700c8e929c903412
Julian Scheid <julians37@gmail.com>
2023-04-17 20:12:30 +0200

    Add package header

--- bitpack.el
+++ bitpack.el
@@ -2,6 +2,13 @@
 
 ;; This is free and unencumbered software released into the public domain.
 
+;; Author: Christopher Wellons <wellons@nullprogram.com>
+;; Version: 1.0.0
+;; Created: 6 Apr 2019
+;; Keywords: c, comm
+;; Homepage: https://github.com/skeeto/bitpack
+;; Package-Requires: ((emacs "24.3"))
+
 ;;; Commentary:
 
 ;; bitdat is similar to the built-in bindat package. However, this
